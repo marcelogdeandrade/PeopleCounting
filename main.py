9c5d322a3bcba6e235246ed9b5c8c3f43a8e262b
marcelogdeandrade <marcelogdeandrade@gmail.com>
2018-10-01 14:55:59 -0300

    removes main file and adds src folder

--- main.py
+++
@@ -1,7 +0,0 @@
-from codebook import Codebook
-
-#codebooks = Codebook.train_codebooks()
-#Codebook.save_codebooks(codebooks)
-
-codebooks = Codebook.load_codebooks()
-Codebook.test_codebooks(codebooks)
