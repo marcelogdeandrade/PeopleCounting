121142cb47ec3aab2885783c53ec5a4c7e26177a
marcelogdeandrade <marcelogdeandrade@gmail.com>
2018-10-01 15:11:56 -0300

    adds foreground/background example image

--- src/count_people.py
+++ src/count_people.py
@@ -2,7 +2,7 @@ from codebook import Codebook
 import cv2 as cv
 import numpy as np
 
-FILE_NAME = "./img.png"
+FILE_NAME = "./examples/img.png"
 
 img = cv.imread(FILE_NAME, cv.CV_8UC1)
 count = Codebook.count_people(img)
